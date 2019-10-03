32de765a5b1741bcc9eec93175d3139329712ca6
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-03 13:49:28 -0500

    Moved determine cost of character into a separate method to make ifs only append if possible

--- pencil_durability/pencil.py
+++ pencil_durability/pencil.py
@@ -4,15 +4,28 @@ class Pencil:
 
     def write(self, text, paper):
         for char in text:
-            if char.isspace():
-                paper.append(char)
-            elif char.isupper() and self.durability >= 2:
-                paper.append(char)
-                self.durability -= 2
-            elif self.durability >= 1:
-                paper.append(char)
-                self.durability -= 1
-            else:
-                paper.append(' ')
-        # paper.append(text)
+            self._write_character(char, paper)
+
+    def _write_character(self, char, paper):
+        cost = Pencil._determine_cost_of_character(char)
+        if char.isspace():
+            paper.append(char)
+        elif char.isupper() and self.durability >= 2:
+            paper.append(char)
+        elif self.durability >= 1:
+            paper.append(char)
+        else:
+            paper.append(' ')
+
+        self.durability -= cost
+
+    @staticmethod
+    def _determine_cost_of_character(char):
+        if char.isspace():
+            return 0
+
+        if char.isupper():
+            return 2
+
+        return 1
 
