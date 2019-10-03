95be47cdecbb3afc550349c2991b644c99138d26
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-03 10:38:59 -0500

    Making cost method static as it doesn't need to be an instance method

--- pencil_durability/pencil.py
+++ pencil_durability/pencil.py
@@ -3,11 +3,12 @@ class Pencil:
         self.durability = 20
 
     def write(self, text, paper):
-        self.durability -= self._get_durability_cost_of_text(text)
+        self.durability -= Pencil._get_durability_cost_of_text(text)
 
         paper.append(text)
 
-    def _get_durability_cost_of_text(self, text):
+    @staticmethod
+    def _get_durability_cost_of_text(text):
         cost = len(text)
         if text == 'A':
             cost += 1
