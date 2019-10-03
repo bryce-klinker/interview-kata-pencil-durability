448e4bd02b9af8c35bf56aad3bfd783652dcb1d7
Bryce Klinker <bklinker@pillartechnology.com>
2019-10-03 08:55:26 -0500

    Testing framework and base of project looks good

---
+++ pavement.py
@@ -0,0 +1,12 @@
+from paver.easy import *
+import paver.doctools
+from paver.setuputils import setup
+
+setup(
+    name="PencilDurability",
+    packages=['pencil_durability'],
+    version="1.0",
+    url="http://idk.com/",
+    author="Bryce Klinker",
+    author_email="bryce.a.klinker@accenture.com"
+)
